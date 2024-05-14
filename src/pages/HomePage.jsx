d1be966ddd2cdb4e38c911d4e750771a30bca91d
Plo4i <midag@abv.bg>
2024-05-15 00:54:49 +0200

    Added profile picture and finished styling the current profile card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
-import { auth } from '../firebase';
+import { auth, firestore } from '../firebase'; // Make sure to import firestore
+import { doc, getDoc } from 'firebase/firestore';
 import { Icon } from '@iconify/react';
 
+import DefaultProfileImage from '../assets/images/default-profile-image.jpg';
+
 // Components imports
-import Footer from '../components/Footer'
+import Footer from '../components/Footer';
 import Nav from "../components/Nav";
 
-
 export default function HomePage() {
-    // This is the functional component for the HomePage
-
     const [user] = useAuthState(auth);
+    const [profileData, setProfileData] = useState(null);
+
+    useEffect(() => {
+        const fetchUserProfile = async () => {
+            if (user) {
+                const userDocRef = doc(firestore, 'users', user.uid);
+                const userSnap = await getDoc(userDocRef);
+
+                if (userSnap.exists()) {
+                    setProfileData(userSnap.data());
+                } else {
+                    console.log("No such document!");
+                }
+            }
+        };
+
+        fetchUserProfile();
+    }, [user]);
 
     return (
         <>
@@ -21,17 +39,20 @@ export default function HomePage() {
                 <section className="profile" id="profile">
 
                     <div className='current-profile-card'>
-                        <div className='profile-picture'></div>
-                        <div className='profile-name'>Elizabet Aleksieva</div>
-                        <a href="">view profile</a>
+                        {/* Display the profile picture */}
+                        <div className='profile-picture'>
+                            <img src={profileData?.profileImageURL || DefaultProfileImage } alt="Profile"  />
+                        </div>
+                        <div className='profile-name'>{profileData?.name}</div>
+                        <a className='view-profile-button' href="">view profile</a>
                         <div className='followers-container'>
                             <div className='followers'>
                                 <p>Followers:</p>
-                                <p>56</p>
+                                <p className='number-of-followers'>56</p>
                             </div>
                             <div className='following'>
                                 <p>Following:</p>
-                                <p>67</p>
+                                <p className='number-of-following'>67</p>
                             </div>
                         </div>
                         <div className='saved-items'><a href=""><Icon icon="mdi:favorite-box-outline" /> Saved items</a></div>
@@ -46,7 +67,7 @@ export default function HomePage() {
                 <section className='infinite-scroll' id='infinite-scroll'>
 
                     <div className='feed-card'>
-                        
+
                     </div>
 
                 </section>
