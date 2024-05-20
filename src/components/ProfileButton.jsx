b73723deb82c45590ac25167bd566eda513b31b5
Plo4i <midag@abv.bg>
2024-05-20 18:04:40 +0200

    Created logout dropdown and started working on settigns and privacy dropdown

--- src/components/ProfileButton.jsx
+++ src/components/ProfileButton.jsx
@@ -14,8 +14,11 @@ export default function ProfileButton() {
     const [user] = useAuthState(auth);
 
     const [profileData, setProfileData] = useState(null);
-    // State for dropdown visibility and delete confirmation modal
+
+    // State for dropdown visibility
     const [isDropdownOpen, setDropdownOpen] = useState(false);
+    const [isUserLoggingOut, setIsUserLoggingOut] = useState(false);
+    const [isSettingsAndPrivacyOpen, setIsSettingsAndPrivacyOpen] = useState(false);
     const [isDeleteConfirmationOpen, setDeleteConfirmationOpen] = useState(false);
 
     // Hook for navigating to different routes
@@ -73,7 +76,32 @@ export default function ProfileButton() {
 
     const closeProfileDropdown = () => {
         setDropdownOpen(false);
+        setIsUserLoggingOut(false);
     };
+
+    // Toggle user logout
+
+    const toggleUserLogout = () => {
+        setDropdownOpen(false);
+        setIsUserLoggingOut(!isUserLoggingOut);
+    }
+
+    const goBackFromLogoutQuestion = () => {
+        setIsUserLoggingOut(false);
+        setDropdownOpen(!isDropdownOpen)
+    }
+
+    // Toggle settings and privacy dropdown
+    
+    const toggleSettingsAndPrivacy = () => {
+        setDropdownOpen(false);
+        setIsSettingsAndPrivacyOpen(!isSettingsAndPrivacyOpen);
+    }
+
+    const goBackFromSettingsAndPrivacy = () => {
+        setIsSettingsAndPrivacyOpen(false);
+        setDropdownOpen(!isDropdownOpen)
+    }
 
     // Close dropdown when clicking outside
     const handleDocumentClick = (event) => {
@@ -101,7 +129,7 @@ export default function ProfileButton() {
                 </div>
                 {/* Dropdown content */}
                 {isDropdownOpen && (
-                    <div className="profile-dropdown" id="profileDropdown">
+                    <div className="profile-dropdown" id="profile-dropdown">
                         <div className="dropdown-content">
                             <div className='title-container'>
                                 <div className='profile-name-container'>
@@ -112,10 +140,50 @@ export default function ProfileButton() {
                             </div>
                             <div className='profile-dropdown-buttons-container'>
                                 <NavLink className='dropdown-button' id='dropdown-button' to={`/${user?.uid}/profile`} onClick={() => setDropdownOpen(false)}><Icon icon="bi:person-fill" /> View Profile</NavLink>
-                                <button className='dropdown-button' id='dropdown-button' onClick={handleLogOut}><Icon icon="solar:settings-bold" /> Settings & Privacy</button>
+                                <button className='dropdown-button' id='dropdown-button' onClick={toggleSettingsAndPrivacy}><Icon icon="solar:settings-bold" /> Settings & Privacy</button>
                                 <NavLink className='dropdown-button' id='dropdown-button' to={`/${user?.uid}`} onClick={() => setDropdownOpen(false)}><Icon icon="material-symbols:help" /> Help Center</NavLink>
                                 <button className='dropdown-button' id='dropdown-button' onClick={handleLogOut}><Icon icon="material-symbols:feedback-outline-rounded" /> Give a feedback</button>
-                                <button className='dropdown-button' id='dropdown-button' onClick={handleLogOut}><Icon icon="ic:round-log-out" /> Log Out</button>
+                                <button className='dropdown-button' id='dropdown-button' onClick={toggleUserLogout}><Icon icon="ic:round-log-out" /> Log Out</button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                {isUserLoggingOut && (
+                    <div className="logout-dropdown" id="logout-dropdown">
+                        <div className="dropdown-content">
+                            <div className='title-container'>
+                                <div className='go-back-container'>
+                                    <Icon icon="typcn:arrow-left" onClick={goBackFromLogoutQuestion} />
+                                    <h2 className='profile-name'>Log Out</h2>
+                                </div>
+                                <Icon icon="carbon:close-filled" onClick={closeProfileDropdown} />
+                            </div>
+                            <div className='logout-question'>
+                                <p><b>{user?.displayName}</b>, are you sure that you want to Log out from your ErasmusLinkUp profile?</p>
+                            </div>
+                            <div className='profile-dropdown-buttons-container'>
+                                <button className='dropdown-button' id='dropdown-button' onClick={handleLogOut}><span>Continue Log Out</span></button>
+                            </div>
+                        </div>
+                    </div>
+                )}
+
+                {isSettingsAndPrivacyOpen && (
+                    <div className="settings-and-privacy-dropdown" id="settings-and-privacy-dropdown">
+                        <div className="dropdown-content">
+                            <div className='title-container'>
+                                <div className='go-back-container'>
+                                    <Icon icon="typcn:arrow-left" onClick={goBackFromSettingsAndPrivacy} />
+                                    <h2 className='profile-name'>Settings & Privacy</h2>
+                                </div>
+                                <Icon icon="carbon:close-filled" onClick={closeProfileDropdown} />
+                            </div>
+                            <div className='logout-question'>
+                                <p><b>{user?.displayName}</b>, are you sure that you want to Log out from your ErasmusLinkUp profile?</p>
+                            </div>
+                            <div className='profile-dropdown-buttons-container'>
+                                <button className='dropdown-button' id='dropdown-button' onClick={handleLogOut}><span>Continue Log Out</span></button>
                             </div>
                         </div>
                     </div>
